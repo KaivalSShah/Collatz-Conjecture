eec85e4cbb3fa27b7137c5f96514c6a9014f50c9
Kaival145 <kaival.s.shah@gmail.com>
2022-02-03 22:09:41 -0800

    Fix: write to spreadsheet

--- src/alg.py
+++ src/alg.py
@@ -5,6 +5,7 @@ run_till = int(input("Ending positive integer: "))
 run_for = run_till - number
 original_num = int(number)
 i=0
+y_axis_actual = []
 iterations = {}
 while i >= 0 and original_num <= run_till:
     def add_values_in_dict(sample_dict, key, list_of_values):
@@ -12,7 +13,7 @@ while i >= 0 and original_num <= run_till:
         sample_dict[key].extend(list_of_values)
         return sample_dict
     if number == 1:
-        iterations = add_values_in_dict(iterations, str(original_num), [str(i)])
+        iterations = add_values_in_dict(iterations, str(original_num), str(i))
         original_num += 1
         i = 0
         number = original_num
@@ -25,6 +26,12 @@ while i >= 0 and original_num <= run_till:
 print(iterations)
 x_axis = list(iterations.keys())
 y_axis = list(iterations.values())
-df = pd.DataFrame(data=iterations)
-df = (df.T)
-df.to_excel('./data/'+str(run_for)+'_collatz.xlsx')
+print(x_axis)
+print(y_axis)
+for value in y_axis:
+    print(value)
+    individual_value = value[0]
+    y_axis_actual.append(individual_value)
+graph_dict = {""}
+df = pd.DataFrame({'number': x_axis, 'iterations': y_axis_actual})
+df.to_excel('./data/collatz.xlsx')

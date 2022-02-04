7297e17a543a23d367a82eecc47d01cdbc53dd9b
Kaival145 <kaival.s.shah@gmail.com>
2022-02-04 06:56:35 -0800

    Update graph.py

--- src/graph.py
+++ src/graph.py
@@ -3,3 +3,10 @@ import plotly.graph_objects as go
 import pandas as pd
 import plotly.express as px
 
+x = ("number")
+y = ("iterations")
+df = pd.read_excel("./data/collatz.xlsx")
+print(df)
+data = [go.Scatter(x=df[x], y=df[y])]
+fig = go.Figure(data)
+plotly.offline.plot(fig, filename = "./data/graph.html")

28f55a6f5b6790680ba6bb3040c76a25e0c51c69
Kaival145 <kaival.s.shah@gmail.com>
2022-02-03 22:41:47 -0800

    Fix: store data spreadsheet

--- src/alg.py
+++ src/alg.py
@@ -5,7 +5,6 @@ run_till = int(input("Ending positive integer: "))
 run_for = run_till - number
 original_num = int(number)
 i=0
-y_axis_actual = []
 iterations = {}
 while i >= 0 and original_num <= run_till:
     def add_values_in_dict(sample_dict, key, list_of_values):
@@ -13,7 +12,7 @@ while i >= 0 and original_num <= run_till:
         sample_dict[key].extend(list_of_values)
         return sample_dict
     if number == 1:
-        iterations = add_values_in_dict(iterations, str(original_num), str(i))
+        iterations = add_values_in_dict(iterations, str(original_num), [str(i)])
         original_num += 1
         i = 0
         number = original_num
@@ -24,14 +23,22 @@ while i >= 0 and original_num <= run_till:
         number = 3*number + 1
         i+=1
 print(iterations)
-x_axis = list(iterations.keys())
-y_axis = list(iterations.values())
+x_axis = []
+y_axis = []
+
+for value_y in list(iterations.values()):
+    individual_value_y = value_y[0]
+    y_axis.append(individual_value_y)
+
+for value_x in list(iterations.keys()):
+    individual_value_x = value_x[0]
+    x_axis.append(individual_value_x)
+
+for i in range(0, len(x_axis)):
+    x_axis[i] = int(x_axis[i])
+    y_axis[i] = int(y_axis[i])
+
 print(x_axis)
 print(y_axis)
-for value in y_axis:
-    print(value)
-    individual_value = value[0]
-    y_axis_actual.append(individual_value)
-graph_dict = {""}
-df = pd.DataFrame({'number': x_axis, 'iterations': y_axis_actual})
+df = pd.DataFrame({'number': x_axis, 'iterations': y_axis})
 df.to_excel('./data/collatz.xlsx')

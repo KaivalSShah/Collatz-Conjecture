1b3d5c689759c18859081eb04b84d62e5c028112
Kaival145 <kaival.s.shah@gmail.com>
2022-02-04 07:38:16 -0800

    Update scatter.py

--- src/scatter.py
+++ src/scatter.py
@@ -3,8 +3,6 @@ import plotly.graph_objects as go
 import pandas as pd
 import plotly.express as px
 
-x = ("number")
-y = ("iterations")
 df = pd.read_excel("./data/collatz.xlsx")
 data = px.scatter(df, x="number", y="iterations", title="Scatter Plot")
 fig = go.Figure(data)
